317ed8b2fc433776d0acaabf4ccadaad5ed4318f
MUGESH R <108133070+63816138@users.noreply.github.com>
2024-08-25 20:58:56 +0530

    Add files via upload

---
+++ week7-protfoli-completed/action.js
@@ -0,0 +1,29 @@
+
+const contentshow=()=>{
+    const contentvar1=document.querySelector(".list1");
+    const contentvar2=document.querySelector(".list2");
+    const contentvar3=document.querySelector(".list3");
+    const contentvar4=document.querySelector(".list4");
+    const contentvar5=document.querySelector(".list5");
+    const contentvar6=document.querySelector(".list6");
+    contentvar1.style="display:block;";
+    contentvar2.style="display:block;";
+    contentvar3.style="display:block;";
+    contentvar4.style="display:block;";
+    contentvar5.style="display:block;";
+    contentvar6.style="display:block;";
+}
+const contentshowhide=()=>{
+    const contentvar1=document.querySelector(".list1");
+    const contentvar2=document.querySelector(".list2");
+    const contentvar3=document.querySelector(".list3");
+    const contentvar4=document.querySelector(".list4");
+    const contentvar5=document.querySelector(".list5");
+    const contentvar6=document.querySelector(".list6");
+    contentvar1.style="display:none;";
+    contentvar2.style="display:none;";
+    contentvar3.style="display:none;";
+    contentvar4.style="display:none;";
+    contentvar5.style="display:none;";
+    contentvar6.style="display:none;";
+}
